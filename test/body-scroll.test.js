7570d36f2a423143db194cb6c1442b94abdb0aaf
Daniele Fioroni <dfioroni91@gmail.com>
2019-10-12 07:17:42 +0200

    Unit tests init

--- test/body-scroll.test.js
+++ test/body-scroll.test.js
@@ -1,9 +1,41 @@
-describe('body scroll', function() {
-    before(function() {});
+let lib;
 
-    after(function() {});
+before(function() {
+    lib = window.bodyScroll;
+});
 
-    it('do stuff', function(done) {
+after(function() {
+    lib = null;
+});
+
+describe('Basic interface', function() {
+    it('Should expose lock method', function(done) {
+        expect('lock' in lib).to.be.true;
+
+        done();
+    });
+
+    it('Should expose unlock method', function(done) {
+        expect('unlock' in lib).to.be.true;
+
+        done();
+    });
+
+    it('Should expose isLocked method', function(done) {
+        expect('isLocked' in lib).to.be.true;
+
+        done();
+    });
+
+    it('Should expose toggle method', function(done) {
+        expect('toggle' in lib).to.be.true;
+
+        done();
+    });
+
+    it('Should expose update method', function(done) {
+        expect('update' in lib).to.be.true;
+
         done();
     });
 });
