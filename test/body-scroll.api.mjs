310e6a76e794b6bde853162da3ec45dce5575572
Daniele Fioroni <dfioroni91@gmail.com>
2020-02-02 19:10:45 +0100

    more unit test coverage

---
+++ test/body-scroll.api.mjs
@@ -0,0 +1,35 @@
+import * as Scroll from "../src/body-scroll.scroll.mjs";
+
+const div = document.createElement("div");
+div.style.height = "9999px";
+
+beforeEach(() => document.body.append(div));
+
+afterEach(() => div.remove());
+
+describe("Api provider module", () => {
+    // TODO:
+    /*it('"isLocked" method ...', done => {
+        done();
+    });
+
+    // TODO:
+    it('"lock" method ...', done => {
+        done();
+    });
+
+    // TODO:
+    it('"unlock" method ...', done => {
+        done();
+    });
+
+    // TODO:
+    it('"update" method ...', done => {
+        done();
+    });
+
+    // TODO:
+    it("resize ...", done => {
+        done();
+    });*/
+});
--- test/body-scroll.mjs
+++ test/body-scroll.mjs
@@ -1,37 +1,30 @@
-// TODO: include dist/body-scroll.js and test
-// - lock exposure
-// - unlock exposure
-// - isLocked exposure
-// - toggle exposure
-// - update exposure
-
-describe("Public Api surface", function() {
-    it("should be defined globally", function(done) {
+describe("Public Api surface", () => {
+    it("should be defined globally", done => {
         expect("bodyScroll" in window).to.be.true;
         expect(typeof window.bodyScroll === "object").to.be.true;
 
         done();
     });
 
-    it('should expose "isLocked" method', function(done) {
+    it('should expose "isLocked" method', done => {
         expect("isLocked" in window.bodyScroll).to.be.true;
 
         done();
     });
 
-    it('should expose "update" method', function(done) {
+    it('should expose "update" method', done => {
         expect("update" in window.bodyScroll).to.be.true;
 
         done();
     });
 
-    it('should expose "lock" method', function(done) {
+    it('should expose "lock" method', done => {
         expect("lock" in window.bodyScroll).to.be.true;
 
         done();
     });
 
-    it('should expose "unlock" method', function(done) {
+    it('should expose "unlock" method', done => {
         expect("unlock" in window.bodyScroll).to.be.true;
 
         done();
