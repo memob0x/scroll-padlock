927492c152b50ffa7c7dfd75e60aa187b4a633ce
memob0x <dfioroni91@gmail.com>
2020-12-04 13:32:37 +0100

    demo files prepared

--- demo/src/app.vue
+++ demo/src/app.vue
@@ -1,23 +1,68 @@
 <template>
-    <div id="app">
+    <div class="app">
         <console />
+        
+        <div class="app__body">
+            <button>
+                open layer
+            </button>
 
-        hello there
+            <button>
+                toggle scroll
+            </button>
+
+            <sample-text v-for="index in 3" :key="index" />
+
+            <scroller />
+            
+            <sample-text v-for="index in 17" :key="index" />
+
+            <floater />
+
+            <layer />
+        </div>
     </div>    
 </template>
 
 <script>
     import console from './console.vue';
+    import scroller from './scroller.vue';
+    import sampleText from './sample-text.vue';
+    import floater from './floater.vue';
+    import layer from './layer.vue';
 
     export default {
         name: 'app',
 
         components: {
-            console
+            console,
+            sampleText,
+            scroller,
+            floater,
+            layer
         }
     };
 </script>
 
 <style lang="scss">
     @import '../../node_modules/reset-css/sass/_reset.scss';
+
+    * {
+        box-sizing: border-box;
+    }
+</style>
+
+<style lang="scss" scoped>
+    .app {
+        font-family: 'Roboto', sans-serif;
+        line-height: 1.6;
+        font-size: 16px;
+
+        &__body {
+            width: 1200px;
+            max-width: 100%;
+            margin: 0 auto;
+            padding: 0 20px;
+        }
+    }
 </style>
---
+++ demo/src/scroller.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="scroller">
+        asd
+    </div>    
+</template>
+
+<script>
+    export default {
+        name: 'scroller'
+    };
+</script>
+
+<style lang="scss" scoped>
+</style>
