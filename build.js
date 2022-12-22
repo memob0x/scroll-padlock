48fe8d1088574ceb284c036914a0b9209dc2f30f
memob0x <dfioroni91@gmail.com>
2022-12-22 21:46:23 +0100

    updated node modules, restored terser build plugin

--- build.js
+++ build.js
@@ -1,9 +1,7 @@
 import { rollup } from 'rollup';
 import { getBabelOutputPlugin as rollupBabel } from '@rollup/plugin-babel';
 import rollupGzip from 'rollup-plugin-gzip';
-// NOTE: temporarily removed terser till https://github.com/rollup/plugins/issues/1366 is closed
-// import rollupTerser from '@rollup/plugin-terser';
-import fs from 'fs/promises';
+import rollupTerser from '@rollup/plugin-terser';
 import { resolve } from 'path';
 
 const BUNDLES_PRESETS = [
@@ -41,32 +39,22 @@ const BUNDLES_PRESETS = [
 const pathRoot = resolve('.');
 
 (async () => {
-  const [
-    babelConfigStream = 0,
-
-    rollupResult = 1,
-  ] = await Promise.all([
-    // TODO: check why this is not read by default
-    fs.readFile(`${pathRoot}/babel.config.json`),
-
-    rollup({
-      input: `${pathRoot}/src/padlock.js`,
-    }),
-  ]);
-
-  const babelConfig = JSON.parse(babelConfigStream);
+  const rollupResult = await rollup({
+    input: `${pathRoot}/src/padlock.js`,
+  });
 
   return Promise.all(BUNDLES_PRESETS.reduce((accumulator, { format, min, babel }) => {
     const plugins = [];
 
-    // NOTE: temporarily removed terser till https://github.com/rollup/plugins/issues/1366 is closed
-    // if (min) {
-    // plugins.push(rollupTerser());
-    // }
+    if (min) {
+      plugins.push(rollupTerser());
+    }
 
     if (babel) {
       plugins.push(rollupBabel({
-        ...babelConfig,
+        presets: [
+          '@babel/preset-env',
+        ],
 
         comments: false,
 
